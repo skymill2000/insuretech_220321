9cc5d061b9281dd72bd0e831d9f2f705c3f6b1ea
skymill2000 <skymill2000@gmail.com>
2022-03-22 15:46:51 +0900

    AIA 치아 보험 조회 완성

--- scraping/dental_scraper.py
+++ scraping/dental_scraper.py
@@ -20,10 +20,30 @@ def rePlaceData(value):
 def getAIAData(name, birth, gender):
     driver.get(
         'https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html#')
+    driver.implicitly_wait(3)
     if gender == 0:
+        maleButton = driver.find_element_by_xpath(
+            '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
+        maleButton.click()
         # 남자를 클릭하라
     else:
-        # 여자 버튼을 클릭하라
+        femaleButton = driver.find_element_by_xpath(
+            '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[1]')
+        femaleButton.click()
+
+    birthInput = driver.find_element_by_xpath('//*[@id="aia644363719"]')
+    birthInput.send_keys("19"+birth)
+
+    confirmButton = driver.find_element_by_xpath('//*[@id="btn806817556"]')
+    confirmButton.click()
+
+    driver.implicitly_wait(3)
+    htmlResult = driver.find_element_by_xpath(
+        '//*[@id="premium-by-timespan-value"]')
+    print(htmlResult.text)
+
+    # 여자 버튼을 클릭하라
+    # 여기에 작성
 
 
-getAIAData("이름", "000101", 0)
+getAIAData("이름", "700101", 0)
