bfb12493a29f967e42aca6b6813992677aff95b7
skymill2000 <skymill2000@gmail.com>
2022-03-22 16:25:29 +0900

    웹 서버 실행 방법 denetal_web_server 에서 node bin/www 입력

---
+++ dental_web_server/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/result', function (req, res, next) {
+  res.render('result')
+})
+
+module.exports = router;
