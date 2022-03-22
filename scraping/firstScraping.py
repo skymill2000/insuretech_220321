2d671b8a6364ef6101132589e3eef3ff2d25f709
skymill2000 <skymill2000@gmail.com>
2022-03-22 11:41:20 +0900

    뉴스 제목 / 요약 스크래핑 코드

--- scraping/firstScraping.py
+++ scraping/firstScraping.py
@@ -2,4 +2,10 @@ from selenium import webdriver
 driver = webdriver.Chrome('./chromedriver')
 # 크롬을 통해서 스크래핑을 진행 크롬 드라이버 로딩
 
-driver.get('https://naver.com')
+driver.get('https://www.mk.co.kr/news/culture/view/2022/03/259303/?utm_source=naver&utm_medium=newsstand')
+title = driver.find_element_by_xpath('//*[@id="top_header"]/div/div/h1')
+titleFromClassName = driver.find_element_by_class_name('top_title')
+subtitle = driver.find_element_by_xpath('//*[@id="top_header"]/div/div/h2')
+print(title.text)
+print(subtitle.text)
+print(titleFromClassName.text)
