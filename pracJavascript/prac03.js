8dd6cee4f5331eef615c53a6f797a982ee4ddfc1
skymill2000 <skymill2000@gmail.com>
2022-03-23 11:08:51 +0900

    실습3 사칙연산 기능 선언

---
+++ pracJavascript/prac03.js
@@ -0,0 +1,28 @@
+function myFunction(p1, p2) {
+  //여러가지 프로그래밍
+  return p1 + p2;
+}
+
+const plus = (p1, p2) => {
+  return p1 + p2;
+};
+
+const minus = (p1, p2) => {
+  return p1 - p2;
+};
+
+const multi = (p1, p2) => {
+  return p1 * p2;
+};
+
+const div = (p1, p2) => {
+  return p1 / p2;
+};
+
+//실습 3 minus, multi, div => 빼기, 곱하기 나누기 기능 추가 바랍니다.
+
+console.log(myFunction(5, 5));
+console.log(plus(5, 9));
+console.log(minus(5, 9));
+console.log(multi(5, 9));
+console.log(div(5, 9));
