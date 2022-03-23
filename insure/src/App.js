a5d4a46ee326c106e7f62a04bf429305ee021d5a
skymill2000 <skymill2000@gmail.com>
2022-03-23 14:48:03 +0900

    useState 를 통한 데이터 변경후 리렌더링 실습

--- insure/src/App.js
+++ insure/src/App.js
@@ -6,11 +6,6 @@ import InputTest from "./components/InputTest";
 function App() {
   return (
     <>
-      <h1>안녕하세요</h1>
-      <h3>리액트 입니다</h3>
-      <Header title={"테스트 페이지"}></Header>
-      <Header title={"테스트 페이지"}></Header>
-      <Header title={"테스트 페이지"}></Header>
       <InputTest></InputTest>
     </>
   );
--- insure/src/components/InputTest.js
+++ insure/src/components/InputTest.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 //rafce 입력
 const InputTest = () => {
+  //   let text = "test";
+  const [text, setText] = useState("test");
+
   const handleChange = (event) => {
     const { value } = event.target;
-    console.log(value);
+    setText(value);
   };
   return (
     <div>
+      <p>{text}</p>
       <input onChange={handleChange}></input>
     </div>
   );
