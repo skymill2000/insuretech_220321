07d469563f2fe165424bfc320e87c4b4fc480978
skymill2000 <skymill2000@gmail.com>
2022-03-23 11:17:10 +0900

    배열 선언 / 사용 실습

---
+++ pracJavascript/prac04.js
@@ -0,0 +1,6 @@
+const array = [1, "test", { name: "test" }, 123.42312];
+console.log(array[0]);
+
+//in java
+// public String [] StringArray = new String[5];
+// public ArrayList(String) arrayListString = new ArrayList(String);
