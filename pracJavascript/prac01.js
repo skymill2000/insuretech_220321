7764d3550e4c05b70123d17297f6238d0aa91d87
skymill2000 <skymill2000@gmail.com>
2022-03-23 10:32:16 +0900

    자바스크립트 변수 실습

---
+++ pracJavascript/prac01.js
@@ -0,0 +1,31 @@
+var a = "hello";
+var a = "bye";
+
+let b = "test";
+let c = "test2";
+b = "test3";
+
+const d = "test";
+let name = "name";
+let age = 15;
+let height = 144.32;
+
+const method = () => {
+  console.log("hello");
+};
+
+const array = [a, b, c];
+
+const obj = {
+  name: "test",
+  age: 14,
+};
+
+console.log(name, age, height);
+
+console.log(b);
+
+// java
+// private String name = "name";
+// private int age = 15;
+// private double height = 133.5;
