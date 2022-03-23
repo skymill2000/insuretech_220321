ad81bda1143d30124931899058e01a11dcd140b4
skymill2000 <skymill2000@gmail.com>
2022-03-23 11:28:42 +0900

    배열과 반복문 실습

--- pracJavascript/prac04.js
+++ pracJavascript/prac04.js
@@ -1,6 +1,17 @@
 const array = [1, "test", { name: "test" }, 123.42312];
-console.log(array[0]);
-
 //in java
 // public String [] StringArray = new String[5];
 // public ArrayList(String) arrayListString = new ArrayList(String);
+
+for (let i = 0; i < array.length; i++) {
+  let element = array[i];
+  console.log(element);
+}
+
+for (element of array) {
+  console.log(element);
+}
+
+array.map((data) => {
+  console.log(data);
+});
