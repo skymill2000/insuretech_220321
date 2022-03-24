ce8dbfbf997f958c55f10676881cf7e983dbceaf
skymill2000 <skymill2000@gmail.com>
2022-03-24 16:07:43 +0900

    병원 목록 조회 페이지 스테이트 추가

--- insure/src/pages/HospitalPage.js
+++ insure/src/pages/HospitalPage.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import SearchResult from "../components/Hospital/SearchResult";
 import SearchInput from "../components/Hospital/SearchInput";
 
 const HospitalPage = () => {
+  const [hospitalList, setHospitalList] = useState([]);
+  const [searchInputText, setSearchInputText] = useState("");
+
   return (
     <div>
       <Header title={"병원정보 목록"} />
