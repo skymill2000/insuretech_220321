d4a421f1c4d5dafe4a769af50bbf429bb03216b4
skymill2000 <skymill2000@gmail.com>
2022-03-24 10:14:38 +0900

    axios 실습

--- insure/src/App.js
+++ insure/src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import InputTest from "./components/InputTest";
 import ListComponent from "./components/ListComponent";
+import AxiosTest from "./pages/AxiosTest";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Header />}></Route>
         <Route path="/test" element={<InputTest />}></Route>
         <Route path="/test2" element={<ListComponent />}></Route>
+        <Route path="/axios" element={<AxiosTest />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ insure/src/pages/AxiosTest.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AxiosTest = () => {
+  const handleClick = () => {};
+  return <button onClick={handleClick}>요청 전송</button>;
+};
+
+export default AxiosTest;
