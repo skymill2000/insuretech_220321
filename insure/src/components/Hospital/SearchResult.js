105cbef921456cbc6ad5b6003251e4f4c000f079
skymill2000 <skymill2000@gmail.com>
2022-03-24 16:15:00 +0900

    데이터 조회 요청 완료

--- insure/src/components/Hospital/SearchResult.js
+++ insure/src/components/Hospital/SearchResult.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SearchResult = ({ phamacyList }) => {
+const SearchResult = ({ hospitalList }) => {
   return (
     <div>
-      {phamacyList.map((data, index) => {
+      {hospitalList.map((data, index) => {
         return (
           <p key={index}>
             {data.yadmNm} {data.addr}
--- insure/src/pages/HospitalPage.js
+++ insure/src/pages/HospitalPage.js
@@ -2,15 +2,40 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import SearchResult from "../components/Hospital/SearchResult";
 import SearchInput from "../components/Hospital/SearchInput";
+import axios from "axios";
 
 const HospitalPage = () => {
   const [hospitalList, setHospitalList] = useState([]);
   const [searchInputText, setSearchInputText] = useState("");
 
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchInputText(value);
+  };
+  const handleClick = () => {
+    const option = {
+      method: "GET",
+      url: "http://apis.data.go.kr/B551182/hospInfoService1/getHospBasisList1",
+      params: {
+        ServiceKey:
+          "uiu3ZzNzDB04UbxOtOL1atH04WOtxB5WSKkPbaCASVHbwgcsIPwHA5Qp6xOmSe6fzCnUVifZcfTXDkgNegv4qQ==",
+        sidoCd: 110000,
+        yadmNm: searchInputText,
+      },
+    };
+
+    axios(option).then(function ({ data }) {
+      console.log(data);
+      setHospitalList();
+    });
+  };
   return (
     <div>
       <Header title={"병원정보 목록"} />
-      <SearchInput></SearchInput>
+      <SearchInput
+        handleChange={handleChange}
+        handleClick={handleClick}
+      ></SearchInput>
       {/* <SearchResult></SearchResult> */}
       {
         //searchInput
