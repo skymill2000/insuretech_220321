32aee1c7e6b876796d184ce81a88fe0c83b3af74
skymill2000 <skymill2000@gmail.com>
2022-03-24 14:08:18 +0900

    중간 완성

--- insure/src/pages/PhamacyPage.js
+++ insure/src/pages/PhamacyPage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import SearchInput from "../components/Phamacy/SearchInput";
 import SearchResult from "../components/Phamacy/SearchResult";
 
 const PhamacyPage = () => {
+  const [phamacyList, setPhamacyList] = useState([]);
+  const [searchInputText, setSearchInputText] = useState("");
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchInputText(value);
+  };
+
+  const handleClick = () => {
+    axios.get(``).then(function ({ data }) {
+      setPhamacyList(data.articles);
+    });
+  };
   return (
     <div>
       <Header title={"약국 검색"}></Header>
