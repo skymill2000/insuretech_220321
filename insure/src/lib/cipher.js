d7a850351d834f05dbc9e9e2d574305ecabe2b39
skymill2000 <skymill2000@gmail.com>
2022-03-25 13:31:11 +0900

    DB 보험 API 암호화 알고리즘 추가

--- insure/src/lib/cipher.js
+++ insure/src/lib/cipher.js
@@ -1,27 +1,56 @@
-const { createCipheriv, createHash, createHmac } = require("crypto");
+const { createCipheriv, createHmac } = require("crypto");
 const makeAES = (plainText) => {
   const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
   const iv = "dwUgwmwOndnJOmOX";
   const cipher = createCipheriv("aes-256-cbc", key, iv);
   let encrypted = cipher.update(plainText, "utf8", "base64");
   encrypted += cipher.final("base64");
-
   console.log("aes-256-cbc 방식으로 암호화한 값 : ", encrypted);
   return encrypted;
-  // 1. 평문(plainText)을
-  // 2. EncKey, EnvIv 값을 이용하여 AES-CBC-256 방식으로 암호화
-  // 3. 암호화 된 결과를 'Base64' 인코딩하기
-  // 4. return 암호화 된 값
 };
+
+//b3Bu7QBZ0q96+f2GY2NQmaT1XnZElI1+wRdSHBOY01s=
+//b3Bu7QBZ0q96+f2GY2NQmaT1XnZElI1+wRdSHBOY01s=
 
 makeAES("!Kwic123테스트");
 
 const sha256Enc = (plainText) => {
   const secret = "abcdefg";
-  const hash = createHmac("sha256", secret)
-    .update("I love cupcakes")
-    .digest("base64");
+  const hash = createHmac("sha512", secret).update(plainText).digest("base64");
   console.log(hash);
 };
 
-sha256Enc("12341234");
+const hsKey = () => {
+  //const key = appSecretKey + timeSt;
+  //var hmac = crypto.createHmac("sha512", new Buffer.from(key, 'utf-8'));
+  //var signed = hmac.update(new Buffer.from(str, 'utf-8')).digest("base64");
+  //console.log(signed);
+  //return signed
+};
+
+const dbInsureEncrypt = () => {
+  // const timeSt = "" ;
+  // const appSecretKey = "";
+  // const serial = timeSt + appSecretKey;
+  // let secureKey = rpad(serial, 32, '0')
+  // var bytes = crypto.randomBytes(20);
+  // let iv = crypto.randomBytes(16)
+  // console.log(bytes);
+  // const key = crypto.pbkdf2Sync(secureKey, bytes, 70000, 32, 'sha1');
+  // const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+  // cipher.setAutoPadding(false);
+  // console.log('b:' , bytes.toString('base64'))
+  // console.log('iv:', iv.toString('base64'))
+  // let encrypted = cipher.update(pkcs7Pad(ssnnum), 'utf8','base64');
+  // encrypted += cipher.final('base64');
+  // console.log("here :", encrypted)
+  // var encBuf = Buffer.from(encrypted,'base64');
+  // let arr = [bytes, iv, encBuf];
+  // let conBuf = Buffer.concat(arr);
+  // console.log(conBuf.toString('base64'));
+  // // encrypted += conBuf.toString('base64');
+  // console.log(encrypted);
+  // return conBuf.toString('base64');
+};
+
+sha256Enc("qlalfqjsgh123!");
