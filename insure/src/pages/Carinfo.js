b94961333fd143897a6dd91e842c456abc118e0e
skymill2000 <skymill2000@gmail.com>
2022-03-25 14:37:02 +0900

    차량 정보 조회 기능 작성

---
+++ insure/src/pages/Carinfo.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Carinfo = () => {
+  const [carnum, setcarnum] = useState("");
+  const [onwer, setonwer] = useState("");
+  const handleClick = () => {
+    const option = {
+      method: "POST",
+      url: "",
+      headers: {},
+      //header Token Data 추가 => object 형태로 추가
+      json: {},
+    };
+    axios(option).then(({ data }) => {
+      console.log(data.response.body.items.item);
+    });
+  };
+  const handleChangeCarnum = (e) => {
+    const { value } = e.target;
+    setcarnum(value);
+  };
+  const handleChangeOwner = (e) => {
+    const { value } = e.target;
+    setonwer(value);
+  };
+
+  return (
+    <div>
+      차량 번호 <input onChange={handleChangeCarnum}></input>
+      <br></br>
+      소유주 : <input onChange={handleChangeOwner}></input>
+      <br></br>
+      <button onClick={handleClick}>차량 정보 조회하기</button>
+    </div>
+  );
+};
+
+export default Carinfo;
--- insure/src/pages/EncryptPage.js
+++ insure/src/pages/EncryptPage.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
+import encrypt from "../lib/encrypt";
 
 const EncryptPage = () => {
   const [plainText, setPlainText] = useState("");
   const [encryptedText, setencryptedText] = useState("");
+
+  const handleClick = () => {
+    setencryptedText(encrypt.makeAES(plainText));
+  };
+
+  const handleChangePassword = (e) => {
+    const { value } = e.target;
+    setPlainText(value);
+  };
+
   return (
     <div>
       <Header title={"데이터 암호화"}></Header>
-      <input type="text" /> <br />
-      <input type="password" />
+      아이디 : <input type="text" /> <br />
+      비밀번호 : <input type="password" onChange={handleChangePassword} />
       <br />
-      <button>암호화</button>
+      {encryptedText}
+      <button onClick={handleClick}>암호화</button>
     </div>
   );
 };
