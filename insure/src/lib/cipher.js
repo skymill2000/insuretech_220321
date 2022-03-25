f4d6baa65d182a589cc725997fcaee7f60a01b64
skymill2000 <skymill2000@gmail.com>
2022-03-25 13:16:29 +0900

    AES-CBC-256 방식 암호화 완성

--- insure/src/lib/cipher.js
+++ insure/src/lib/cipher.js
@@ -1,11 +1,20 @@
 const { createCipheriv, createHash, createHmac } = require("crypto");
 const makeAES = (plainText) => {
-  //암호화 로직
-  //AES-CBC-256 방식으로 암호화 로직 작성
-  // EncKey : H3uVnnOnNIXXsH63CsgswUsmICSCdVsn
-  // EncIV : dwUgwmwOndnJOmOX
-  // base64 형태로 encoding 하기
+  const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
+  const iv = "dwUgwmwOndnJOmOX";
+  const cipher = createCipheriv("aes-256-cbc", key, iv);
+  let encrypted = cipher.update(plainText, "utf8", "base64");
+  encrypted += cipher.final("base64");
+
+  console.log("aes-256-cbc 방식으로 암호화한 값 : ", encrypted);
+  return encrypted;
+  // 1. 평문(plainText)을
+  // 2. EncKey, EnvIv 값을 이용하여 AES-CBC-256 방식으로 암호화
+  // 3. 암호화 된 결과를 'Base64' 인코딩하기
+  // 4. return 암호화 된 값
 };
+
+makeAES("!Kwic123테스트");
 
 const sha256Enc = (plainText) => {
   const secret = "abcdefg";
